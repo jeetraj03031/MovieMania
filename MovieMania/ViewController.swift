f6fa09a2bcf347b284218fb40a31eb8e0d3d8ad1
Jitendra Rajput <jitendra.rajput@in10media.com>
2020-10-02 09:50:33 +0530

    Initial Commit

---
+++ MovieMania/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMania
+//
+//  Created by EPIC on 02/10/20.
+//  Copyright © 2020 Movie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
